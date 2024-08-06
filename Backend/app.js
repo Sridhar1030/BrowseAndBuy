461eb26492cd39ace63814ac501226bbf9e10a9c
pavanhub-47 <140827035+pavanhub-47@users.noreply.github.com>
2024-08-06 21:26:48 +0530

    first commit

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,9 @@ import chatRoute from "./routes/ChatRoute.js"
 import messageRoute from "./routes/messageRoute.js"
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
