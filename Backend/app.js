bf2b85e83001450d5db2399bfa3fdac0e84e1163
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-08-07 00:28:23 +0530

    Cors changes

--- Backend/app.js
+++ Backend/app.js
@@ -22,9 +22,7 @@ import chatRoute from "./routes/ChatRoute.js"
 import messageRoute from "./routes/messageRoute.js"
 
 // Middleware
-app.use(cors(
-    {origin:"*"}
-));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
