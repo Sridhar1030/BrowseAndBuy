638d5203531fde61d2df3aa5afc4a92b4a031fe7
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-08-10 16:29:31 +0530

    approve items

--- Backend/routes/SellRoute.js
+++ Backend/routes/SellRoute.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import Sell from '../models/Sell.js';
-import User from '../models/User.js'; // Correct import statement
+import express from "express";
+import Sell from "../models/Sell.js";
+import User from "../models/User.js"; // Correct import statement
 
 const router = express.Router();
 
@@ -23,7 +23,9 @@ router.post("/", async (req, res) => {
     try {
         const savedItem = await newSell.save();
         const userId = req.body.userId;
-        await User.findByIdAndUpdate(userId, { $push: { ProductId: savedItem._id } });
+        await User.findByIdAndUpdate(userId, {
+            $push: { ProductId: savedItem._id },
+        });
         console.log("New Sell Item Saved:", savedItem);
         res.status(200).json({
             message: "Saved successfully",
@@ -32,6 +34,27 @@ router.post("/", async (req, res) => {
     } catch (error) {
         console.error("Error saving new sell item:", error);
         res.status(500).json({ message: "Save failed", error });
+    }
+});
+
+router.put("/:id/approve", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const updatedItems = await Sell.findByIdAndUpdate(
+            id,
+            { Approved: true },
+            { new: true }
+        );
+        if(!updatedItems){
+            return res.status(404).json({message:"item not found"})
+        }
+        res.status(200).json({
+            message: "Item approved successfully",
+            item: updatedItem,
+        })
+    } catch (error) {
+        console.error("Error approving item:", error);
+        res.status(500).json({ message: "Approval failed", error });
     }
 });
 
